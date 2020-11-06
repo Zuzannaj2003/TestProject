c88e49ce810ddbf8faea57658d915a62b7c6a5f6
Zuzanna <06251@farnborough.ac.uk>
2020-11-06 10:09:23 +0000

    main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String greeting = "Hello, world!";
@@ -5,6 +6,13 @@ public class Main {
         System.out.println(greeting.length());
         int number = 5;
         System.out.println(number);
+        Scanner user_input = new Scanner(System.in);
+        String word;
+        System.out.print("Input a word: ");
+        word = user_input.next();
+
+        String sReverse = new StringBuilder(word).reverse().toString();
+        System.out.println(sReverse);
     }
 }
 
