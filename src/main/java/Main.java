d72988ec60b5ca9a2eccc39ff0247b553c1c08c1
Zuzanna <06251@farnborough.ac.uk>
2020-11-06 10:34:10 +0000

    main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,6 +24,18 @@ public class Main {
         int privateStudyRemainder = privateStudyMins % 60;
 
         System.out.println("You are expected to do " + privateStudyHours + " hours and " + privateStudyRemainder + " minutes of study");
+
+        Scanner input = new Scanner( System.in );
+        System.out.println("Hello new students!");
+        System.out.println("How would you describe yourself?");
+        String description = input.next();
+        System.out.println("How tall are you in metres?");
+        String height = input.next();
+        System.out.println("You like the course so far?");
+        String course = input.next();
+        System.out.println("How old are you?");
+        String age = input.next();
+
     }
 }
 
